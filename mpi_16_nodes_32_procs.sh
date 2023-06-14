47f2a8f2e9cf1ac72f8d78b8b53fed650723705a
Pouya Haghi <tg867305@login4.stampede2.tacc.utexas.edu>
2023-06-13 20:10:45 -0500

    16 nodes 32 procs (MPI)

---
+++ mpi_16_nodes_32_procs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J lulesh_16_nodes_32_procs
+#SBATCH -o lulesh.o%j
+#SBATCH -e lulesh.e%j
+#SBATCH -A TG-PEB220004
+#SBATCH -p skx-normal
+#SBATCH -N 16
+#SBATCH --ntasks-per-node=32
+#SBATCH -t 01:00:00
+
+# module load gcc/7.1.0
+# module load mkl/18.0.2
+data="./timelord_results"
+max_iter=1
+export OMP_NUM_THREADS=32
+export I_MPI_PIN_PROCESSOR_LIST=allcores
+for ((iter=1; iter<=$max_iter; iter=iter+1))
+do
+    ibrun ./lulesh -s 40 -p -i 500 > $data/lulesh_os_"${iter}"_${SLURM_NTASKS}_small.txt
+done
+
+
